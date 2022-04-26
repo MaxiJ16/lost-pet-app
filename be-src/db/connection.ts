ad6333856da3f56915d574100a59095918b33788
Maxi Jofré <maxijofre.c@gmail.com>
2022-04-26 19:33:51 -0300

    arreglando errores de keys

--- be-src/db/connection.ts
+++ be-src/db/connection.ts
@@ -2,11 +2,11 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize({
   dialect: "postgres",
-  username: "quhxzpkvpnqdrn",
-  password: "83e488332dfeb073b1cc1ab9decebd408cfcbe46f7961838e90b8439add3a8cc",
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
   database: "dd4f7ka0p0t8i6",
   port: 5432,
-  host: "ec2-54-160-109-68.compute-1.amazonaws.com",
+  host: process.env.DATABASE_HOST,
   ssl: true,
   // esto es necesario para que corra correctamente
   dialectOptions: {
