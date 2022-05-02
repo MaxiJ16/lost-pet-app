b7cfae73344cdae567e3a8bbcd80c3fa7d30e891
Maxi Jofré <maxijofre.c@gmail.com>
2022-05-02 20:58:56 -0300

    new changes fe

--- fe-src/state.ts
+++ fe-src/state.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:3000";
-// process.env.API_BASE_URL ||
+const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:3000";
+
 const state = {
   data: {
     user: {
