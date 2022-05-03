ac8b5ea69c7cd9a5519593333de007c376a1386e
Maxi Jofré <maxijofre.c@gmail.com>
2022-05-03 00:46:00 -0300

    modified sndgridapi

--- be-src/lib/sendgrid.ts
+++ be-src/lib/sendgrid.ts
@@ -1,6 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(process.env.SENDGRID_API);
 
 // F PARA ENVIAR POR MAIL EL REPORTE DE LA MASCOTA
 export async function sendEmailReport(reportData) {
