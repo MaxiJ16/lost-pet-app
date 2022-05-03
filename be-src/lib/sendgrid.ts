b3aa2537db5dd144bfa54dacdc95aff0f4f7d705
Maxi Jofré <maxijofre.c@gmail.com>
2022-05-02 23:07:15 -0300

    prueba ak sendgrd

--- be-src/lib/sendgrid.ts
+++ be-src/lib/sendgrid.ts
@@ -1,6 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 
-sgMail.setApiKey(process.env.SENDGRID_APIK);
+sgMail.setApiKey("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");
 
 // F PARA ENVIAR POR MAIL EL REPORTE DE LA MASCOTA
 export async function sendEmailReport(reportData) {
